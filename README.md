6dcc903953c95373d868e390009c78d0aa002251
Lucas Favaro Borsatto <paradigma0621@gmail.com>
2023-03-21 18:56:35 -0300

    Initial project inicialization

---
+++ README.md
@@ -0,0 +1,9 @@
+# Time Flow System 
+Backend and Frontend implementation of software to measure time quality in work.
+
+## Report
+This project is designed to run in *Java 8*.
+
+2023-03-21 - Started Report Microservice project.
+
+
---
+++ src/main/java/com/timeflow/reportjava8/ReportJava8Application.java
@@ -0,0 +1,13 @@
+package com.timeflow.reportjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportJava8Application.class, args);
+	}
+
+}
